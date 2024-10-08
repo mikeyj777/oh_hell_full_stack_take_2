b6a2d1567acdaacbad3ce26bf5a2c91a1983195c
Michael James <mikeyj777@gmail.com>
2024-10-07 21:54:08 -0400

    starting

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Settings from './components/Settings';
+import MainDashboard from './components/MainDashboard';
+import GameDisplay from './components/GameDisplay';
+fd
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
